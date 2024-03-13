2b633400bdd873a37ec34388c34b5194b194d273
philfreshman <philipswiezak@gmail.com>
2024-03-13 11:45:27 +0100

    complete add-activity functionality

--- Corrina-Philip-weather-app/src/App.jsx
+++ Corrina-Philip-weather-app/src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-import './App.css'
-import Form from './components/Form/Form.jsx';
+import { useState } from "react"
+import "./App.css"
+import Form from "./components/Form/Form.jsx"
+import { uid } from "uid"
+
+const initialActivities = [
+  {
+    id: uid(),
+    name: "hello",
+    isForGoodWeather: true,
+  },
+]
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [activities, setActivities] = useState(initialActivities)
+
+  function handleAddActivity(name, isForGoodWeather) {
+    setActivities([
+      ...activities,
+      {
+        id: uid(),
+        name: name,
+        isForGoodWeather: isForGoodWeather,
+      },
+    ])
+  }
 
   return (
-  <>
-    <Form />
-  </>)
+    <>
+      <Form onAddActivity={handleAddActivity} />
+    </>
+  )
 }
 
-export default App  
+export default App
