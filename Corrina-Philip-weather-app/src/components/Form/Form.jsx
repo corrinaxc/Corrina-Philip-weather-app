95f26a6f8f6d709965d22091be9166369075b455
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-13 11:18:57 +0100

    added onSubmit for form

--- Corrina-Philip-weather-app/src/components/Form/Form.jsx
+++ Corrina-Philip-weather-app/src/components/Form/Form.jsx
@@ -1,14 +1,24 @@
 export default function Form( { onAddActivity } ) {
+    function onSubmit(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const name = formData.get("name");
+        const isForGoodWeather = formData.get("weather").isChecked;
+        console.log(name, isForGoodWeather);
+        
+    } 
+    
     return(
         <>
         <h1>Add new Activity:</h1>
-        <form>
+        <form id="activityForm" onSubmit={onSubmit}>
             <label htmlFor="name">Name</label>
-            <input id="name" type="text"></input>
+            <input id="name" type="text" name="name"></input>
             <label htmlFor="isForGoodWeather">Good-weather Activity</label>
-            <input id="isForGoodWeather" type="checkbox"></input>
+            <input id="isForGoodWeather" type="checkbox" name="weather"></input>
             <button type="submit">Submit</button>
-        </form>
+        </form >
         </>
     )
 
