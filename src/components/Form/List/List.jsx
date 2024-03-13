2312ab2ccd8cbdcc27b30b3ff2840e030d9ee8c2
philfreshman <philipswiezak@gmail.com>
2024-03-13 15:00:33 +0100

    add filtering

--- src/components/Form/List/List.jsx
+++ src/components/Form/List/List.jsx
@@ -1,14 +1,16 @@
-
-
-export default function List( { activites } ) {
-    
-    return(
-        <>
-        {activites.map(({id, name, isForGoodWeather}) => (
+export default function List({ activites, isGoodWeather }) {
+  return (
+    <>
+      <h1>
+        {isGoodWeather === true
+          ? "the sun has got his hat on, and so should you"
+          : "bad weather"}
+      </h1>
+      {activites.map(({ id, name }) => (
         <ul className="activity-list" key={id}>
-            <li>{name}</li>
+          <li>{name}</li>
         </ul>
-    ))}
-     </>
-    );
+      ))}
+    </>
+  )
 }
