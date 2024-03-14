8d0103c551d1f6c1e8782e3218ee449cd9d30449
Corrina Cornish <corrina.cornish@gmail.com>
2024-03-14 14:25:41 +0100

    added required for text input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -20,7 +20,7 @@ export default function Form({ onAddActivity }) {
       <form id="activity-form" onSubmit={onSubmit}>
         <h2>Add a New Activity:</h2>
         <label htmlFor="name">Name</label>
-        <input id="name" type="text" name="name"></input>
+        <input id="name" type="text" name="name" required></input>
         <label id="isForGoodWeather-label" htmlFor="isForGoodWeather">Good-weather Activity</label>
         <input id="isForGoodWeather" type="checkbox" name="weather"></input>
         <button id="submit-button" type="submit">Submit</button>
